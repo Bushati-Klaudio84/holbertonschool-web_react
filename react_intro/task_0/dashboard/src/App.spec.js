99308d69f051a356d7617d216d0c015912685d8a
Bushati-Klaudio84 <klaudio1@gmail.com>
2024-09-16 16:00:25 +0200

    add

---
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import App from "./App.jsx";
+
+test('should contain a <p/> element with specific text, <h1/>, and an <img/>', () => {
+  render(<App />);
+
+  const currentYear = new Date().getFullYear();
+  const regex = new RegExp(`copyright ${currentYear} - holberton school`, 'i');
+
+  const paragraphElement = screen.getByText('Login to access the full dashboard');
+  const footerParagraphElement = screen.getByText(regex);
+  const headingElement = screen.getByRole('heading', { name: /School dashboard/i });
+  const imgElement = screen.getByRole('img');
+
+  expect(paragraphElement).toBeInTheDocument();
+  expect(footerParagraphElement).toBeInTheDocument();
+  expect(headingElement).toBeInTheDocument();
+  expect(imgElement).toBeInTheDocument();
+});
